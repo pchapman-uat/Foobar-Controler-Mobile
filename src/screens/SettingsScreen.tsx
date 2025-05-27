e330acfee3904041c1e590d94b7f3af2b8218479
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2025-05-27 14:10:06 -0700

    Fix settings theme style

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,19 +1,20 @@
 import { Picker } from "@react-native-picker/picker";
 import { AppContext } from "AppContext";
 import { AppTheme } from "classes/Settings";
-import { createStyle } from "managers/StyleManager";
+import { createStyle, useStyles } from "managers/StyleManager";
 import { getColor } from "managers/ThemeManager";
 import { useContext, useEffect, useState } from "react";
 import { View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 import ThemeContext from "ThemeContext";
 import { Button } from "react-native-elements";
 
 export default function SettingsScreen() {
     const [theme, setTheme] = useState<AppTheme>()
     const Theme = useContext(ThemeContext);
-    const Styles = createStyle(Theme.theme, 'Main')
+    const Styles = useStyles('Main')
     const ctx = useContext(AppContext);
+
     const onSave = () => {
         console.log("saving", theme)
         const PROPS = ctx.Settings.PROPS;
@@ -26,16 +27,14 @@ export default function SettingsScreen() {
     }, []);
 
     return (
-        <SafeAreaProvider>
-            <SafeAreaView style={Styles.Main.container}>
-                <View>
-                    <Picker style={Styles.Main.picker} onValueChange={setTheme} dropdownIconColor={getColor(Theme.theme, 'textPrimary')} mode='dropdown' selectedValue={theme}>
-                        <Picker.Item label="Light" value={AppTheme.Light} />
-                        <Picker.Item label="Dark" value={AppTheme.Dark} />
-                    </Picker>
-                </View>
-                <Button style={Styles.Main.button} title='Save' onPress={onSave}/>
-            </SafeAreaView>
-        </SafeAreaProvider>
+        <SafeAreaView style={Styles.Main.container}>
+            <View>
+                <Picker style={Styles.Main.picker} onValueChange={setTheme} dropdownIconColor={getColor(Theme.theme, 'textPrimary')} mode='dropdown' selectedValue={theme}>
+                    <Picker.Item label="Light" value={AppTheme.Light} />
+                    <Picker.Item label="Dark" value={AppTheme.Dark} />
+                </Picker>
+            </View>
+            <Button buttonStyle={Styles.Main.button} title='Save' onPress={onSave}/>
+        </SafeAreaView>
     )
 }
