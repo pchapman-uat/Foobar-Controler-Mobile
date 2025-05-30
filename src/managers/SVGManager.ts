ec8cb7118cbc2659441d08e6886f3ae20e547ce7
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2025-05-30 13:01:24 -0700

    Remove unused dependency and asset

--- src/managers/SVGManager.ts
+++ src/managers/SVGManager.ts
@@ -1,8 +1,7 @@
-import PlaceholderSVG from '../assets/placeholder.svg'
 import ConnectionSVG from '../assets/navigation/connection.svg'
 import LibrarySVG from '../assets/navigation/library.svg'
 import NowPlayingSVG from '../assets/navigation/nowPlaying.svg'
 import PlaybackQueueSVG from '../assets/navigation/playbackQueue.svg'
 import MenuSVG from '../assets/menu.svg'
 
-export {PlaceholderSVG, ConnectionSVG, LibrarySVG, NowPlayingSVG, PlaybackQueueSVG, MenuSVG}
+export { ConnectionSVG, LibrarySVG, NowPlayingSVG, PlaybackQueueSVG, MenuSVG}
