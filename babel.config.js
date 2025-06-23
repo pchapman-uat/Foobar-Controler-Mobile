ffd872f4874aa4d4e63d9b466b3e5a724102603e
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2025-06-23 10:00:47 -0700

    Resolve Building Issues

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,23 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
-        'module-resolver',
+        "module-resolver",
         {
-          root: ['./src'],
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+          root: ["./src"],
+          alias: {
+            screens: "./src/screens",
+            classes: "./src/classes",
+            managers: "./src/managers",
+            elements: "./src/elements",
+            enum: "./src/enum",
+            helpers: "./src/helpers",
+            hooks: "./src/hooks",
+            style: "./src/style",
+            assets: "./src/assets"
+          },
         },
       ],
     ],
