9202ff295b74ae8491ea9f680655ba137f72b47b
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2025-11-09 12:56:57 -0700

    Restart app when settings reset

--- src/elements/modal/ResetAllSettingsModal.tsx
+++ src/elements/modal/ResetAllSettingsModal.tsx
@@ -2,12 +2,14 @@ import { AppContextType } from "AppContext";
 import React from "react";
 import { Text, View } from "react-native";
 import { Button } from "react-native-elements";
+import RNRestart from "react-native-restart";
 import ModalTypes from "./ModalTypes";
 export type RASProps = ModalTypes<"resetAll">;
 
 const resetAll = (ctx: AppContextType) => {
 	// TODO: Close Application
 	ctx.Settings.resetAll();
+	RNRestart.restart();
 };
 
 export const RASActions: RASProps["action"] = {
