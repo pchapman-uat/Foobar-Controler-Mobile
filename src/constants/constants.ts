bca0e587b60c5bb6cdf0c96437f583580989d22b
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2025-12-12 23:12:10 -0700

    Update Version

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 import { GITHUB_REPO_NAME, GITHUB_REPO_OWNER } from "./constants.github";
 
-export const APP_VERSION = "v0.4.0";
+export const APP_VERSION = "v0.5.0";
 export const APP_NAME = "AstroTune";
 export const REPO_URL = `https://github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}`;
 export const RELEASES_URL = `${REPO_URL}/releases`;
